b4bae434bf0e87cc359c215cdcab26502a480859
lukasz lesiuk <lukasz.lesiuk.2@gmail.com>
2020-07-21 13:51:40 +0200

    created customer class and sql script to create customer table(not populated)

--- src/main/java/com/codecool/app/App.java
+++ src/main/java/com/codecool/app/App.java
@@ -1,4 +1,4 @@
-package com.codecool;
+package com.codecool.app;
 
 /**
  * Hello world!
---
+++ src/main/java/com/codecool/customer/Customer.java
@@ -0,0 +1,67 @@
+package com.codecool.customer;
+
+public class Customer {
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String emailAddress;
+    private String city;
+    private String street;
+//    apartment no as a string to accommodate for 19/2 etc;
+    private String apartmentNo;
+    private boolean isLoggedIn;
+
+    public Customer(int customerId, String firstName, String lastName, String phoneNumber,
+                    String emailAddress, String city, String street, String apartmentNo) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.city = city;
+        this.street = street;
+        this.apartmentNo = apartmentNo;
+        this.isLoggedIn = false;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getApartmentNo() {
+        return apartmentNo;
+    }
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        isLoggedIn = loggedIn;
+    }
+}
