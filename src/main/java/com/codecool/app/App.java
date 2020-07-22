954d748226e0164c250e394c268c0e7f14cde439
lukasz lesiuk <lukasz.lesiuk.2@gmail.com>
2020-07-22 17:05:33 +0200

    WIP on recover all clients

--- src/main/java/com/codecool/app/App.java
+++ src/main/java/com/codecool/app/App.java
@@ -4,11 +4,14 @@ import com.codecool.customer.Customer;
 import com.codecool.dao.CustomerDAO;
 import com.codecool.dao.CustomerDAOPSQL;
 
+import java.util.List;
+
 public class App {
     public static void main( String[] args ) {
         //temp code
         CustomerDAO customerDao = new CustomerDAOPSQL();
         Customer selectedCustomer = customerDao.getCustomer("JgsMz0d1");
+//        List<Customer> customerList = customerDao.getAllCustomers();
         System.out.println("ama");
     }
 }
--- src/main/java/com/codecool/dao/CustomerDAOPSQL.java
+++ src/main/java/com/codecool/dao/CustomerDAOPSQL.java
@@ -10,32 +10,51 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class CustomerDAOPSQL implements CustomerDAO {
+    private String url = "jdbc:postgresql://localhost:5432/aspirations_shop";
+    private String user = "postgres";
+    private String password = "sMuGa1@1";
+    private final int ID_POSITION = 0;
+    private final int FIRST_NAME_POSITION = 1;
+    private final int LAST_NAME_POSITION = 2;
+    private final int PHONE_NO_POSITION = 3;
+    private final int EMAIL_POSITION = 4;
+    private final int CITY_POSITION = 5;
+    private final int STREET_POSITION = 6;
 
     @Override
     public Customer getCustomer(String id) {
-        String inputString = retriveByIDToString(id);
+        String inputString = retrieveQueryResponseAsString("SELECT * FROM customers WHERE id = ?", id);
         List<String> attributesList = new ArrayList<>(Arrays.asList(inputString.split(", ")));
 
         try {
             //id retrieval unnecessary since id was declared at the start
-            String firstName = attributesList.get(1);
-            String lastName = attributesList.get(2);
-            String phone = attributesList.get(3);
-            String email = attributesList.get(4);
-            String city = attributesList.get(5);
-            String street = attributesList.get(6);
+            String firstName = attributesList.get(FIRST_NAME_POSITION);
+            String lastName = attributesList.get(LAST_NAME_POSITION);
+            String phone = attributesList.get(PHONE_NO_POSITION);
+            String email = attributesList.get(EMAIL_POSITION);
+            String city = attributesList.get(CITY_POSITION);
+            String street = attributesList.get(STREET_POSITION);
             return (new Customer(id, firstName, lastName, phone, email, city, street));
         } catch (java.lang.IndexOutOfBoundsException e) {
             throw new IllegalArgumentException("Id did not match any element from DB");
-//            System.out.println("Id did not match any element from DB");
-//            System.exit(1);
-//            return null;
         }
     }
 
     @Override
     public List<Customer> getAllCustomers() {
-        return null;
+        int customersQty = getRecordsQty("customers");
+        List<Customer> peopleList = new ArrayList<Customer>();
+
+        for (int i = 0; i < customersQty; i++) {
+            String customerString = retrieveQueryResponseAsString("SELECT * FROM customers LIMIT ? OFFSET ?;",
+                                                                    String.valueOf(i), String.valueOf(i-1));
+            List<String> attributesList = new ArrayList<>(Arrays.asList(customerString.split(", ")));
+
+            String customerId = attributesList.get(ID_POSITION);
+            peopleList.add(getCustomer(customerId));
+        }
+        return peopleList;
+//        return null;
     }
 
     @Override
@@ -53,18 +72,32 @@ public class CustomerDAOPSQL implements CustomerDAO {
 
     }
 
-    private String retriveByIDToString(String id) {
-        String url = "jdbc:postgresql://localhost:5432/aspirations_shop";
-        String user = "postgres";
-        String password = "sMuGa1@1";
 
-        String query = "SELECT * FROM customers WHERE id = ?";
+//    private String retrieveQueryResponseAsString(String query, String... insertValues) {
+//        ResultSet rs = retrieveQueryResponse(String query, String... insertValues);
+//        String queryResponse = "";
+//        try {
+//            queryResponse = convertResultSetToString(rs);
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+//        return queryResponse;
+//    }
 
-        String queryResponse = "";
+    private String retrieveQueryResponseAsString(String query, String... insertValues) {
+
+         String queryResponse = "";
+
         try (Connection con = DriverManager.getConnection(url, user, password);
-             PreparedStatement pst = con.prepareStatement(query)) {
-             pst.setString(1, id);
+            PreparedStatement pst = con.prepareStatement(query)) {
+            int index = 1;
+            for (String insertValue : insertValues)
+             pst.setString(index, insertValue);
+
              try (ResultSet rs = pst.executeQuery()) {
+                 System.out.println("---");
+                 System.out.println("---");
+                 System.out.println("---");
                  queryResponse = convertResultSetToString(rs);
              }
         } catch (SQLException ex) {
@@ -72,6 +105,12 @@ public class CustomerDAOPSQL implements CustomerDAO {
             lgr.log(Level.SEVERE, ex.getMessage(), ex);
         }
         return  queryResponse;
+    }
+
+
+    private int getRecordsQty(String tableName) {
+        String retrievedString = retrieveQueryResponseAsString("SELECT COUNT(*) FROM ? ", tableName);
+        return Integer.parseInt(retrievedString);
     }
 
     private String convertResultSetToString(ResultSet resultSet) throws SQLException {
@@ -90,5 +129,4 @@ public class CustomerDAOPSQL implements CustomerDAO {
         }
         return queryResponse;
     }
-
 }
