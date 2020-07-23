0f5b51c5c90a6e59dcc72546a055b2dddee094c9
irrehaare <wjtkwilk@gmail.com>
2020-07-23 22:46:53 +0200

    Implement register functonality.

--- src/main/java/com/codecool/access/RegistrationController.java
+++ src/main/java/com/codecool/access/RegistrationController.java
@@ -1,13 +1,96 @@
 package com.codecool.access;
 
+import com.codecool.dao.CustomerDAO;
+import com.codecool.dao.CustomerDAOPSQL;
+import com.codecool.customer.Customer;
+import com.codecool.IDGenerator;
+
+import java.util.Scanner;
+
 public class RegistrationController {
     RegistrationView view;
+    Scanner scan;
+    CustomerDAO customerDAO;
+    IDGenerator idGen;
 
     RegistrationController() {
+        this.customerDAO = new CustomerDAOPSQL("/home/irrehaare/CodeCool/Java/AspirationsShop/src/main/resources/");
         this.view = new RegistrationView();
+        this.idGen = new IDGenerator();
+        this.scan = new Scanner(System.in);
     }
 
     public void run(){
         view.clear();
+        Customer newUser = createCustomer();
+        customerDAO.addCustomer(newUser);
+    }
+
+    private Customer createCustomer() {
+        String id = idGen.generateID();
+        String firstName = input("Enter your first name");
+        String lastName = input("Enter your last name");
+        String phoneNumber = input("Enter your phone number");
+        String emailAddress = input("Enter your email address");
+        System.out.println("Address:");
+        String city = input("City: ");
+        String street = input("Street and apartment number:");
+        System.out.println();
+        String password = getPassword();
+        return new Customer(id, firstName, lastName, phoneNumber, emailAddress, city, street, password);
+    }
+
+    private String input(String message){
+        System.out.print(message + ": ");
+        String input = scan.nextLine();
+        view.clear();
+        return input;
+    }
+
+    private String getPassword() {
+        String password;
+        do {
+            password = input("Enter password");
+            while (!isPasswordGood(password)) {
+                input("This password didn't meet the policy requirements.\nEnter password");
+            }
+            if (!input("Enter password again").equals(password)){
+                view.print("Passwords didn't match. Press Enter and try again.");
+                scan.nextLine();
+            }
+        } while (!input("Enter password again").equals(password));
+
+        return Integer.toString(password.hashCode());
+    }
+
+    private boolean isPasswordGood(String password) {
+        boolean result = true;
+        if (password.length()<8 || !hasDifferentChars(password)){
+            result = false;
+        }
+        return result;
+    }
+
+    private boolean hasDifferentChars(String password) {
+        int hasDigit = 0;
+        int hasUpper = 0;
+        int hasLower = 0;
+        int hasSpecial = 0;
+        char[] chars = password.toCharArray();
+
+        for (char aChar : chars) {
+            if (Character.isDigit(aChar)) {
+                hasDigit = 1;
+            } else if (Character.isLowerCase(aChar)){
+                hasLower = 1;
+            } else if (Character.isUpperCase(aChar)){
+                hasUpper = 1;
+            } else if (!Character.isLetterOrDigit(aChar)){
+                hasSpecial = 1;
+            }
+        }
+
+        int sum = hasDigit + hasLower + hasUpper + hasSpecial;
+        return sum >= 3;
     }
 }
--- src/main/java/com/codecool/access/RegistrationView.java
+++ src/main/java/com/codecool/access/RegistrationView.java
@@ -1,7 +1,36 @@
 package com.codecool.access;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class RegistrationView {
     void clear(){
         System.out.print("\033[H\033[2J");
+        printAnsiFile("ANSI_registration");
+    }
+
+    void printAnsiFile(String filename){
+        try {
+            String filepath = "src/main/resources/" + filename;
+            File myObj = new File(filepath);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    void println(String text){
+        System.out.println(text);
+    }
+
+    void print(String text){
+        System.out.print(text);
     }
 }
