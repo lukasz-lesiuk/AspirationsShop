0b57318871b5fa1e4cd7c682a5b9a7914d261633
Krzysztof Chromiec <krzysztof.chromiec1@gmail.com>
2020-07-23 16:19:05 +0200

    new commit

--- src/main/java/com/codecool/transactionDAO/SQLTransactionDAO.java
+++ src/main/java/com/codecool/transactionDAO/SQLTransactionDAO.java
@@ -1,9 +1,30 @@
 package com.codecool.transactionDAO;
 
-public class SQLTransactionDAO {
+import com.codecool.transaction.Transaction;
+
+import java.util.List;
+
+public class SQLTransactionDAO implements TransactionDAO {
+    @Override
+    public void addTransaction(Transaction transaction) {
+
+    }
+
+    @Override
+    public Transaction getTransaction(String transactionID) {
+        return null;
+    }
+
+    @Override
+    public List<Transaction> getAllTransactions() {
+        return null;
+    }
+
+    @Override
+    public void deleteTransaction(String transactionID) {
+
+    }
     // łączenie z bazą danych
     // pobieranie setu rekordów na podstawie: customerID
     // przyjmowanie listy obiektów (produktów biorących udział w transakcji) i dodawanie ich do bazy danych
-    //
-    // update???
 }
--- src/main/java/com/codecool/transactionDAO/TransactionDAO.java
+++ src/main/java/com/codecool/transactionDAO/TransactionDAO.java
@@ -5,9 +5,9 @@ import com.codecool.transaction.Transaction;
 import java.util.List;
 
 public interface TransactionDAO {
-    void addNewTransactionRecord(List<Transaction> transactionRecords);
-    // getAllTransactions() powinno zwracać listę składającą się z list transakcji powiązanych przez transactionID
-    List<List<Transaction>> getAllTransactions();
-    List<Transaction> getTransaction(String transactionID);
-    void deleteTransaction(String transactionID);
+    public void addTransaction(Transaction transaction);
+    public Transaction getTransaction(String transactionID);
+    public List<Transaction> getAllTransactions();
+    public void deleteTransaction(String transactionID);
+    // UPDATE
 }
